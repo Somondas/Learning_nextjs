1826c24365528ccb5f8a16c787e1097aaea8526f
Somon Das <somondas067@gmail.com>
2022-10-29 19:52:07 -0700

    1. Learn about file based routing 2. Refresh some js and react concepts 3. Created a new folder for a new project..

---
+++ first-nextjs-project/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div style={{width: "300px",height: "400px",textAlign: 'center',}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis quibusdam libero, assumenda numquam eaque repellat, quia rerum pariatur in ex a laboriosam? Iste, reiciendis ratione voluptatum aliquid alias aliquam consequuntur.</div>
+  )
+}
+
+export default about
