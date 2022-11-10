ff9309b3ee2b65bffece2ede4ae457405bcfa212
Somon Das <somondas067@gmail.com>
2022-11-09 19:14:28 -0800

    1. Added some styling in blogpost

--- hunting-coder/pages/blogpost/[slug].js
+++ hunting-coder/pages/blogpost/[slug].js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import styles from "../../styles/Blog.module.css";
 
 const slug = () => {
     const router = useRouter();
     const {slug} = router.query;
 
   return (
-    <div>{slug}</div>
+    <div className={styles.blogPost}>
+      <h1>Title: {slug}</h1>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam commodi numquam animi officiis nemo aliquam veritatis modi rerum officia! Sunt dolores illum dolor modi distinctio iste cumque aut quos vel.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui amet cum officia eos possimus rerum similique sequi quisquam voluptatum quibusdam quaerat earum, dolores eum, nobis, magni dicta. Assumenda, rem fugit!</p>
+      </div>
   )
 }
 
