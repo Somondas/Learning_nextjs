6f1a8dfc0fdb0ebcccda499b225d2b2b45886759
Somon Das <somondas067@gmail.com>
2022-11-14 18:49:59 -0800

    1. Pushed all the data to allBlogs. 2. Used the API endpoint

--- hunting-coder/pages/api/blogs.js
+++ hunting-coder/pages/api/blogs.js
@@ -1,11 +1,26 @@
 // * Valid slug example: http://localhost:3000/api/blogs?slug=how-to-learn-javascript
 import * as fs from "fs";
-export default function handler(req, res) {
-  fs.readFile(`blogdata/${req.query.slug}.json`, "utf-8",(err, data)=>{
-    if(err){
-      res.status(500).json({error: "No such blog found"})
-    }
-    console.log(req.query.slug);
-    res.status(200).json(JSON.parse(data))
-  })
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let myFile;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    console.log(item);
+    myFile = await fs.promises.readFile(("blogdata/" + item), "utf-8")
+    // console.log(myFile);
+    allBlogs.push(JSON.parse(myFile))
+    
+  }
+  res.status(200).json(allBlogs)
+
+
+
+  // fs.readdir(`blogdata`, "utf-8",(err, data)=>{
+    // if(err){
+    //   res.status(500).json({error: "No such blog found"})
+    // }
+    // console.log(req.query.slug);
+  
+  // })
 }
