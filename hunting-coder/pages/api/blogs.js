1b09376ee2db80bc9f5fb0a92354bb63250797e1
Somon Das <somondas067@gmail.com>
2022-11-17 21:20:31 -0800

    Working on fetching blogpost data.

--- hunting-coder/pages/api/blogs.js
+++ hunting-coder/pages/api/blogs.js
@@ -6,21 +6,9 @@ export default async function handler(req, res) {
   let allBlogs = [];
   for (let index = 0; index < data.length; index++) {
     const item = data[index];
-    // console.log(item);
     myFile = await fs.promises.readFile(("blogdata/" + item), "utf-8")
-    // console.log(myFile);
     allBlogs.push(JSON.parse(myFile))
     
   }
   res.status(200).json(allBlogs)
-
-
-
-  // fs.readdir(`blogdata`, "utf-8",(err, data)=>{
-    // if(err){
-    //   res.status(500).json({error: "No such blog found"})
-    // }
-    // console.log(req.query.slug);
-  
-  // })
 }
