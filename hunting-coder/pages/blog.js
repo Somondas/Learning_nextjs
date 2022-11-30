612b6f5991c7db2c3cf35747a353eb028be90645
Somon Das <somondas067@gmail.com>
2022-11-29 21:20:44 -0800

    Today, I learned about Server Side Rendering and about getServerSideProps() It was fun today.

--- hunting-coder/pages/blog.js
+++ hunting-coder/pages/blog.js
@@ -5,28 +5,29 @@ import styles from '../styles/Blog.module.css'
 
 // Todo: Step 1: Collect all the files from blogdata directory
 // Todo: Itrate through them and display
-const Blog = () => {
+const Blog = (props) => {
   // tip: Use useStateSnippit instead of useState in the suggestions
-  const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3000/api/blogs").then((a) => {
-      return a.json();})
-      .then((parsed) => {
-      setBlogs(parsed)
-    }).catch((e) => console.log(e)) 
-  }, [])
+  const [blogs, setBlogs] = useState(props.allBlogs);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/api/blogs").then((a) => {
+  //     return a.json();
+  //   })
+  //     .then((parsed) => {
+  //       setBlogs(parsed)
+  //     }).catch((e) => console.log(e))
+  // }, [])
   return (
     <main className={styles.main}>
       <h1>Blogs</h1>
       <div className={styles.grid}>
         <div className="blogs">
           {
-            blogs.map((blogItems,index)=>{
-              return(
+            blogs.map((blogItems, index) => {
+              return (
                 <div className="blogItems" key={index}>
-                <Link href={`/blogpost/${blogItems.slug}`}><h2>{blogItems.title}</h2></Link>
-                <p>{blogItems.content.substr(0, 300)+ "..."}</p>
-              </div>
+                  <Link href={`/blogpost/${blogItems.slug}`}><h2>{blogItems.title}</h2></Link>
+                  <p>{blogItems.content.substr(0, 300) + "..."}</p>
+                </div>
               )
             })
           }
@@ -36,5 +37,14 @@ const Blog = () => {
     </main>
   )
 }
+// >> Server Side Rendering
 
+export async function getServerSideProps(context){
+  let data = await fetch("http://localhost:3000/api/blogs")
+  let allBlogs = await data.json()
+  return{
+    props: {allBlogs}, // will be passed to the page component as props
+  }
+  
+}
 export default Blog
--- hunting-coder/pages/blogpost/[slug].js
+++ hunting-coder/pages/blogpost/[slug].js
@@ -1,37 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import styles from "../../styles/Blog.module.css";
-//tip: Use the browser search bar for removing the \n from a paragraph
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'
+import styles from '../../styles/Blog.module.css';
 
-// |||||||                                                                                    
+// Step 1: Find the file corresponding to the slug
+// Step 2: Populate them inside the page
 
-// Todo: Step 1: Find the file corresponding to the slug
-// Todo: Step 2: Populate them inside the Page
 const slug = () => {
-  const [blog, setBlog] = useState()
-  const router = useRouter();
-  useEffect(() => {
-    if (router.isReady) return;
-    const { slug } = router.query;
-    fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
-      return a.json();
-    })
-      .then((parsed) => {
-      setBlog(parsed)
-    }).catch((e) => console.log(e))
-  
-   
-  }, [router.isReady])
-  
-  
 
-  return (
-    <div className={styles.blogPost}>
-      <h1>Title: {blog && blog.title}</h1> 
-      <hr />
-      <p>{blog &&blog.content}</p>
-    </div>
-  )
-}
+    const [blog, setBlog] = useState();
 
-export default slug
+    const router = useRouter();
+    useEffect(() => {
+        if (!router.isReady) return;
+        const { slug } = router.query;
+        fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+            return a.json();
+        })
+            .then((parsed) => {
+                setBlog(parsed)
+            })
+    }, [router.isReady])
+
+    return <div className={styles.main}>
+        <main className={styles.blogPost}>
+            <h1>{blog && blog.title}</h1>
+            <hr />
+            <div>
+                {blog && blog.content}
+            </div>
+        </main>
+    </div>;
+};
+
+export default slug;
