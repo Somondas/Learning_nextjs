bb74f2a40d09a0fc10f0a9e2ef819bfed429719b
Somon Das <somondas067@gmail.com>
2023-07-03 18:52:34 +0530

    Working on fetching data

--- codeswears/src/pages/tshirts.js
+++ codeswears/src/pages/tshirts.js
@@ -1,7 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import mongoose, { connect } from "mongoose";
+import handler from './api/hello';
+import Product from '../models/Product';
 
-function Tshirts() {
+function Tshirts({ products }) {
+  console.log(products);
   return (
     <div className=' md:w-3/4 md:mx-auto'>
       <section className="text-gray-600 body-font">
@@ -9,9 +13,9 @@ function Tshirts() {
           <div className="flex flex-wrap -m-4">
             <div className="lg:w-1/4 md:w-1/2 p-4 w-full m-5 shadow-lg">
               <Link href={"/product/while-alive-eat-sleep-code"}>
-                <a className="block relative rounded overflow-hidden L-Affiliate-Tagged">
+                <Link href={""} className="block relative rounded overflow-hidden L-Affiliate-Tagged">
                   <img alt="ecommerce" className=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/51R70m41beL._UX679_.jpg" />
-                </a>
+                </Link>
                 <div className="mt-4 text-center md:text-left">
                   <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
                   <h2 className="text-gray-900 title-font text-lg font-medium">DUDEME While Alive Eat Sleep Code</h2>
@@ -19,78 +23,8 @@ function Tshirts() {
                 </div>
               </Link>
             </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full m-5 shadow-lg">
-              <Link href={"/product/while-alive-eat-sleep-code"}>
-                <a className="block relative rounded overflow-hidden L-Affiliate-Tagged">
-                  <img alt="ecommerce" className=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/51R70m41beL._UX679_.jpg" />
-                </a>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">DUDEME While Alive Eat Sleep Code</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full m-5 shadow-lg">
-              <Link href={"/product/while-alive-eat-sleep-code"}>
-                <a className="block relative rounded overflow-hidden L-Affiliate-Tagged">
-                  <img alt="ecommerce" className=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/51R70m41beL._UX679_.jpg" />
-                </a>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">DUDEME While Alive Eat Sleep Code</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full m-5 shadow-lg">
-              <Link href={"/product/while-alive-eat-sleep-code"}>
-                <a className="block relative rounded overflow-hidden L-Affiliate-Tagged">
-                  <img alt="ecommerce" className=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/51R70m41beL._UX679_.jpg" />
-                </a>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">DUDEME While Alive Eat Sleep Code</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full m-5 shadow-lg">
-              <Link href={"/product/while-alive-eat-sleep-code"}>
-                <a className="block relative rounded overflow-hidden L-Affiliate-Tagged">
-                  <img alt="ecommerce" className=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/51R70m41beL._UX679_.jpg" />
-                </a>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">DUDEME While Alive Eat Sleep Code</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full m-5 shadow-lg">
-              <Link href={"/product/while-alive-eat-sleep-code"}>
-                <a className="block relative rounded overflow-hidden L-Affiliate-Tagged">
-                  <img alt="ecommerce" className=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/51R70m41beL._UX679_.jpg" />
-                </a>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">DUDEME While Alive Eat Sleep Code</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full m-5 shadow-lg">
-              <Link href={"/product/while-alive-eat-sleep-code"}>
-                <a className="block relative rounded overflow-hidden L-Affiliate-Tagged">
-                  <img alt="ecommerce" className=" m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/51R70m41beL._UX679_.jpg" />
-                </a>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirt</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">DUDEME While Alive Eat Sleep Code</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
+
+
 
 
           </div>
@@ -98,6 +32,18 @@ function Tshirts() {
       </section>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connection.readyState[0]) {
+    // return handler(req, res)
+    await mongoose.connect(process.env.NEXT_PUBLIC_DATABASE);
+  }
+  let products = await Product.find()
+
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) },
+  }
 }
 
 export default Tshirts
